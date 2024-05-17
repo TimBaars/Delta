28fe47c77452b4ebe85b6863d68f097ff64bdb86
TimBaars <93587744+TimBaars@users.noreply.github.com>
2024-05-17 21:29:31 +0200

    Update constants.dart

--- User_Interface/frontend/lib/util/constants.dart
+++ User_Interface/frontend/lib/util/constants.dart
@@ -1,7 +1,6 @@
-final String STREAMENDPOINT = "rtsp://192.168.178.60:554/user=view&password=123456&channel=1&stream=1";
-final String RMQHOST = "192.168.178.170";
+final String RMQHOST = "192.168.178.107";
 final String RMQUSERNAME = "rabbitmq";
 final String RMQPASSWORD = "orangepi";
-final String WSHOST = "http://192.168.178.170/api";
+final String WSHOST = "http://192.168.178.107/api";
 
 final String WSHOSTDEBUG = "http://localhost:8080";
