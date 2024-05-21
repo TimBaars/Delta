b513af38232e2cff33f3c0ea673d24fb474a153e
TimBaars <93587744+TimBaars@users.noreply.github.com>
2024-05-21 15:56:02 +0200

    Update main.dart

--- User_Interface/frontend/lib/main.dart
+++ User_Interface/frontend/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:frontend/screen/screen_main.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     SystemStatusLogic().init();
