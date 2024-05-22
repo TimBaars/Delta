eae53d4cad0fae93eb5971824d2c7815e455734b
TimBaars <93587744+TimBaars@users.noreply.github.com>
2024-05-22 10:57:47 +0200

    Update constants.dart

--- User_Interface/frontend/lib/util/constants.dart
+++ User_Interface/frontend/lib/util/constants.dart
@@ -1,7 +1,7 @@
 final String RMQHOST = "localhost";
 final String IMAGEHOST = "http://localhost/images";
 final String RMQUSERNAME = "rabbitmq";
-final String RMQPASSWORD = "orangepi";
+final String RMQPASSWORD = "pi";
 final String WSHOST = "http://localhost/api";
 
 final String WSHOSTDEBUG = "http://localhost:8080";
