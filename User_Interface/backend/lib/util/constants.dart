cf3742f77db19639f5e419449f69eec824609d81
TimBaars <93587744+TimBaars@users.noreply.github.com>
2024-05-22 10:55:26 +0200

    Update constants.dart

--- User_Interface/backend/lib/util/constants.dart
+++ User_Interface/backend/lib/util/constants.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
-const String STREAMENDPOINT = "rtsp://192.168.178.60:554/user=view&password=123456&channel=1&stream=1";
-const String RMQHOST = "192.168.178.170";
+const String RMQHOST = "localhost";
 const String RMQUSERNAME = "rabbitmq";
-const String RMQPASSWORD = "orangepi";
+const String RMQPASSWORD = "pi";
 const int RMQPORT = 1883;
