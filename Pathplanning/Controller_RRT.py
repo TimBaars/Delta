746de2b4deff61c0a77751bd64b948dc3baffeb7
Koen van Wijlick <koenvanwijlick@gmail.com>
2024-05-30 13:27:39 +0200

    test3

--- Pathplanning/Controller_RRT.py
+++ Pathplanning/Controller_RRT.py
@@ -36,7 +36,7 @@ class Controller_RRT:
     
     def sendRobotUpdate(self):
         current_position = self.robot_driver.get_current_position()
-        if current_position is None:
+        if current_position is []:
             current_position = [0, 0, 0]
         mapping = ["x", "y", "z"]
         position = {mapping[i]: current_position[i] for i in range(3)}
