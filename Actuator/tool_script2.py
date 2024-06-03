1341df58d71db05c8d04bb97cd55d5b6ca26392c
jpstroh07 <93579089+jpstroh07@users.noreply.github.com>
2024-06-03 17:49:10 +0200

    Update tool_script2.py

--- Actuator/tool_script2.py
+++ Actuator/tool_script2.py
@@ -1,13 +1,9 @@
 import RPi.GPIO as gpio
 import time
 
-in1 = 13
-in2 = 12
-in3 = 11
-in4 = 10
-
-speedPinA = 6
-speedPinB = 5
+in1 = 3
+in2 = 4
+ena = 2
 
 # Setup GPIO mode
 gpio.setmode(gpio.BCM)
@@ -15,23 +11,15 @@ gpio.setmode(gpio.BCM)
 # Setup pin modes
 gpio.setup(in1, gpio.OUT)
 gpio.setup(in2, gpio.OUT)
-gpio.setup(in3, gpio.OUT)
-gpio.setup(in4, gpio.OUT)
-gpio.setup(speedPinA, gpio.OUT)
-gpio.setup(speedPinB, gpio.OUT)
+gpio.setup(ena, gpio.OUT)
 
 # Initial states
 gpio.output(in1, gpio.HIGH)
 gpio.output(in2, gpio.HIGH)
-gpio.output(in3, gpio.HIGH)
-gpio.output(in4, gpio.HIGH)
 
 # Setup PWM
-pwmA = gpio.PWM(speedPinA, 1000) # 1000 Hz frequency
-pwmB = gpio.PWM(speedPinB, 1000) # 1000 Hz frequency
-
-pwmA.start(0) # Start PWM with 0% duty cycle
-pwmB.start(0) # Start PWM with 0% duty cycle
+pwm = gpio.PWM(ena, 100) # 100 Hz frequency
+pwm.start(75) # Start PWM with 75% duty cycle
 
 def mRight(pin1, pin2):
     gpio.output(pin1, gpio.HIGH)
@@ -42,35 +30,19 @@ def mLeft(pin1, pin2):
     gpio.output(pin2, gpio.HIGH)
     
 def mStop(pin1, pin2):
-    gpio.output(pin1, gpio.HIGH)
-    gpio.output(pin2, gpio.HIGH)
-    
-def mSetSpeed(pwm, speedValue):
-    pwm.ChangeDutyCycle(speedValue)
+    gpio.output(pin1, gpio.LOW)
+    gpio.output(pin2, gpio.LOW)
     
 try:
     while True:
         mRight(in1, in2)
-        mRight(in3, in4)
-        
-        '''
-        Assuming analogRead(A0) equivalent is some input function in python
-        For example, a potentiometer connected to an ADC
-        Here, we'll just simulate with a value
-        '''
-        
-        n = 50 # Simulate an analog read value scaled to 0-100
-        mSetSpeed(pwmA, n)
-        mSetSpeed(pwmB, n)
-        time.sleep(1) # Add some delay to simulate the loop
+        time.sleep(1)
         
         mLeft(in1, in2)
-        mLeft(in3, in4)
-
         time.sleep(1)
 
         mStop(in1, in2)
-        mStop(in3, in4)
+        time.sleep(1)
         
 except Exception as e:
     print("Program interrupted:", e)
@@ -79,6 +51,5 @@ except KeyboardInterrupt:
     pass    
 
 finally:
-    pwmA.stop()
-    pwmB.stop()
+    pwm.stop()
     gpio.cleanup()
