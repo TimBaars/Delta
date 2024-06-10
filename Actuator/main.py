ec8f36d208005f28681dbe8501d5fe8ff6646f18
Tim Baars <t.baars@student.fontys.nl>
2024-06-10 14:26:00 +0200

    test slower down movements

--- Actuator/main.py
+++ Actuator/main.py
@@ -69,9 +69,9 @@ def stepper_down(stepper_range):
     gpio.output(direction_pin, ccw_direction)
     for x in range(stepper_range):
         gpio.output(puls_pin, gpio.HIGH)
-        sleep(.001)
+        sleep(.01)
         gpio.output(puls_pin, gpio.LOW)
-        sleep(.0005)
+        sleep(.005)
 
 # Move stepper motor up 
 def stepper_up(stepper_range=0):
@@ -99,9 +99,9 @@ def stepper_up(stepper_range=0):
                 break
             else:
                 gpio.output(puls_pin, gpio.HIGH)
-                sleep(.01)
+                sleep(.001)
                 gpio.output(puls_pin, gpio.LOW)
-                sleep(.005)
+                sleep(.0005)
         
 def down_Logic():
     print("Down logic")
