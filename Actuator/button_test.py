10e4f6d0c51e9cc926a2a161e213774333cacb0c
TimBaars <t.baars@student.fontys.nl>
2024-06-10 13:42:13 +0200

    modified:   button_test.py

--- Actuator/button_test.py
+++ Actuator/button_test.py
@@ -7,8 +7,8 @@ gpio.setmode(gpio.BCM)
 gpio.setup(buttonPin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 while True:
-	print("Loop")
+#	print("Loop")
 	
 	if gpio.input(buttonPin) == gpio.HIGH:
 		print("Button was pressed")
-		break
+#		break
