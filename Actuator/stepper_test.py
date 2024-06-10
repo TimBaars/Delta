25f0968615468d5f7ff2a9fc81a13392ed2e6990
Koen van Wijlick <koenvanwijlick@gmail.com>
2024-06-10 13:10:57 +0200

    stepper test

--- Actuator/stepper_test.py
+++ Actuator/stepper_test.py
@@ -1,11 +1,23 @@
 from time import sleep
 import RPi.GPIO as gpio
+import time
 
 direction_pin = 20
 puls_pin = 21
 cw_direction = 0
 ccw_direction = 1
-buttonPin = 23
+
+# Setup
+gpio.setmode(gpio.BCM)
+buttonPin = 23  # GPIO pin number for GPIO 23
+gpio.setup(buttonPin, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+def read_with_debounce(pin, debounce_time=0.05):
+    current_state = gpio.input(pin)
+    time.sleep(debounce_time)
+    next_state = gpio.input(pin)
+    return current_state if current_state == next_state else None
+
 
 gpio.setmode(gpio.BCM)
 gpio.setup(direction_pin, gpio.OUT)
@@ -26,7 +38,8 @@ try:
             gpio.output(puls_pin, gpio.LOW)
             sleep(.0005)
 
-            if gpio.input(buttonPin) == gpio.HIGH:
+            state = read_with_debounce(buttonPin)
+            if state is True:
                 print("button pressed")
                 break
         
